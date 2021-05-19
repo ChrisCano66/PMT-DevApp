f2744cda043374608bb7227376d0644fb4e21df6
ChrisCano66 <62594781+ChrisCano66@users.noreply.github.com>
2021-05-19 14:58:15 +0200

    Initial Commit

---
+++ src/components/Title.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Title() {
+    return (
+        <div className="title">
+            <h1>Program Manager Tool</h1>
+            <h4>For App Development</h4>
+        </div>
+    )
+}
+
+export default Title
